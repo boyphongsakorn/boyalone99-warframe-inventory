85752cf86d2ea915e1a2b00c8456e640e60def73
Phongsakorn Wisetthon <3013842+boyphongsakorn@users.noreply.github.com>
2026-09-03 08:01:11 +0000

    Add Dockerfile and cron job for loadout updates

---
+++ cron.js
@@ -0,0 +1,52 @@
+const cron = require('node-cron');
+
+const profileUrl = 'https://boyalone99-warframe-inventory.vercel.app/loadout';
+
+cron.schedule('*/5 * * * *', () => {
+	console.log('Checking for loadout updates...');
+	//check if loadout is have been updated from the last time, if yes then update it
+	if (lastLoadoutHash !== null) {
+		fetch(profileUrl, {
+			headers: { accept: 'application/json', 'user-agent': 'warframe-loadout-proxy/1.0' },
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				const currentLoadoutHash = JSON.stringify(data?.Results?.[0]?.LoadOutInventory ?? {});
+				console.log('Current loadout hash:', currentLoadoutHash);
+				console.log('Last loadout hash:', lastLoadoutHash);
+				if (currentLoadoutHash !== lastLoadoutHash) {
+					lastLoadoutHash = currentLoadoutHash;
+					console.log('Loadout has been updated.');
+					const raw = JSON.stringify({
+						"action": {
+							"name": "WarframeChange"
+						}
+					});
+					fetch('http://192.168.31.141:7474/DoAction', {
+						method: 'POST',
+						headers: {
+							'Content-Type': 'application/json'
+						},
+						body: raw
+					})
+						.then((response) => response.json())
+						.then((data) => {
+							console.log('Action response:', data);
+						})
+						.catch((error) => {
+							console.error('Error sending action request:', error);
+						});
+				}
+			})
+	} else {
+		fetch(profileUrl, {
+			headers: { accept: 'application/json', 'user-agent': 'warframe-loadout-proxy/1.0' },
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				lastLoadoutHash = JSON.stringify(data?.Results?.[0]?.LoadOutInventory ?? {});
+				console.log('Current loadout hash:', lastLoadoutHash);
+				console.log('Initial loadout hash set.');
+			})
+	}
+});
